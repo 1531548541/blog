3933e7f4409438eb89e98270d0d15d8549bbdeac
wujie <1531548541@qq.com>
2021-04-24 18:25:03 +0800

    前台加入写博客和编辑功能

--- src/main/java/com/atguigu/blog/controller/BlogController.java
+++ src/main/java/com/atguigu/blog/controller/BlogController.java
@@ -2,6 +2,7 @@ package com.atguigu.blog.controller;
 
 
 import com.atguigu.blog.entity.TBlog;
+import com.atguigu.blog.entity.TUser;
 import com.atguigu.blog.service.TBlogService;
 import com.atguigu.blog.service.TTagService;
 import com.atguigu.blog.service.TTypeService;
@@ -13,8 +14,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.servlet.http.HttpSession;
 import java.util.List;
 
 /**
@@ -59,6 +63,60 @@ public class BlogController {
     public String blog(@PathVariable Long id, Model model) throws NotFoundException {
         model.addAttribute("blog", blogService.getAndConvert(id));
         return "blog";
+    }
+
+    //新增blog
+    @PostMapping("/blog")
+    public String addBlog(TBlog blog, HttpSession session, RedirectAttributes attributes){
+        TUser user = (TUser) session.getAttribute("user");
+        if(user!=null){
+            blog.setUserId(user.getId());
+        }
+        if(blog.getId()==null){
+            //添加
+            blogService.saveBlog(blog);
+            attributes.addFlashAttribute("message", "新增成功");
+        }else{
+            //修改
+            blogService.updateByBlog(blog);
+            attributes.addFlashAttribute("message", "修改成功");
+        }
+        return "redirect:/index";
+    }
+
+    //查看修改blog页面(回显)
+    @GetMapping("/updateBlog/{id}")
+    public String toEditBlog(@PathVariable Long id,Model model){
+        model.addAttribute("blog", blogService.getBlogById(id));
+        model.addAttribute("typeList", typeService.list(null));
+        model.addAttribute("tagList", tagService.list(null));
+        return "blogs-input";
+    }
+
+    //查看新增blog页面
+    @GetMapping("/blog/input")
+    public String toAddBlogs(Model model,HttpSession session){
+        TUser user = (TUser) session.getAttribute("user");
+        if(user!=null){
+            model.addAttribute("blog", new TBlog());
+            model.addAttribute("typeList", typeService.list(null));
+            model.addAttribute("tagList", tagService.list(null));
+            return "blogs-input";
+        }else {
+            return "login";
+        }
+    }
+
+    //删除blog
+    @GetMapping("/blog/delete/{id}")
+    public String deleteBlog(@PathVariable Long id,RedirectAttributes attributes){
+        boolean b = blogService.removeBlogById(id);
+        if(b){
+            attributes.addFlashAttribute("message","删除成功");
+        }else {
+            attributes.addFlashAttribute("message","删除失败");
+        }
+        return "redirect:/index";
     }
 
 }
